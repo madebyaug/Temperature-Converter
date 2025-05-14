97eb2e03f97e012683df2c636b214758bfbce134
August Foreman <47118712+madebyaug@users.noreply.github.com>
2025-05-14 16:12:58 -0400

    revision based on help

--- index.js
+++ index.js
@@ -1,44 +1,57 @@
-// 2.
+//  2.
 
 // Fahrenheit to Celsius Formula
 // (Fahrenheit − 32) * 5/9 = Celsius
 // 5/9 = 1.8
 
-FahrenheitTemp = 90;
-const convertToCelsius = (FahrenheitTemp - 32) * 1.8;
-// console.log(convertToCelsius);
+// We're creating a function called "convertToCelsius"
+// Adding a parameter (value || number) called "Fahrenheit"
+function convertToCelsius(Fahrenheit) {
+  return (Fahrenheit - 32) * 1.8;
+}
 
-// 3.
-function describeTemperature() {
+// If function was limited
+// const convertToCelsius = (FahrenheitTemp - 32) * 1.8;
+
+// This calls the function "convertToCelsius" and give it a "Fahrenheit" value repeatedly
+// console.log(convertToCelsius(8));
+// console.log(convertToCelsius(10));
+// console.log(convertToCelsius(39));
+
+// 3;
+// preforming a function (parameter)
+function describeTemperature(Fahrenheit) {
   // -0
-  if (convertToCelsius < 0) {
-    return `very cold`;
-  } else if (convertToCelsius < 20) {
+  if (convertToCelsius(Fahrenheit) < 0) {
+    console.log(`very cold`, convertToCelsius(Fahrenheit));
+  } else if (convertToCelsius(Fahrenheit) < 20) {
     // 1 - 19
-    return `cold`;
-  } else if (convertToCelsius < 30) {
+    console.log(`cold`, convertToCelsius(Fahrenheit));
+  } else if (convertToCelsius(Fahrenheit) < 30) {
     // 20 - 29
-    return `warm`;
-  } else if (convertToCelsius < 40) {
+    console.log(`warm`, convertToCelsius(temp));
+  } else if (convertToCelsius(Fahrenheit) < 40) {
     // 30 - 39
-    return `hot`;
-  } else if (convertToCelsius >= 40) {
+    console.log(`hot`, convertToCelsius(Fahrenheit));
+  } else if (convertToCelsius(Fahrenheit) >= 40) {
     // +40
-    return `very hot`;
+    console.log(`very hot`, convertToCelsius(Fahrenheit));
   }
 }
 
-const Description = describeTemperature();
-// console.log(Description);
+// // calling a function (arugment)
+const description = describeTemperature(90);
+// why is this producing a console.log when "description" isn't being declared/called?
+// thought it needed the called with the following below?
+// console.log(description);
 
-// 4.
-
+// 4;
 let Question = prompt(
   "Would you like to know your current temperature in Celsius? Simply provide your local temperature below."
 );
 
-if (Question < 0) {
-  return `very cold`;
-} else if (Question < 20) {
-  // 1 - 19
+if (Question) {
+  // Number(Question);
+  parseInt(Question);
+  describeTemperature(Question);
 }
